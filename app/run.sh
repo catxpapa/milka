ada7a848baa9ed242bb2b378e7c1d04c5c8643c2
CATxPAPA <catxpapa@gmail.com>
2025-07-17 07:27:47 +0200

    更新启动脚本，移除MySQL等待和Node.js启动步骤，添加Node.js和npm安装命令

--- app/run.sh
+++ app/run.sh
@@ -2,13 +2,8 @@
 
 echo "启动喵卡 Milka 应用..."
 
-# 设置工作目录
-cd /lzcapp/pkg/content
-
-# 等待MySQL服务启动
-echo "等待MySQL服务启动..."
-sleep 5
-
-# 启动Node.js应用
-echo "启动Node.js服务器..."
-npm start
+cd /lzcapp/pkg/content/app
+apk update
+apk add nodejs npm
+npm install
+npm run start
